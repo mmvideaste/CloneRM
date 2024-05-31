a3af4a55b3cfc8547255c7268addaacd8a9b61c7
Albert Albala <albert@mediatribe.net>
2024-05-30 21:25:48 -0400

    Notre propre counter (#2)

---
+++ docs/scripts/my.js
@@ -0,0 +1,34 @@
+jQuery( document ).ready(function() {
+  // See https://inorganik.github.io/countUp.js/
+  // See https://remysharp.com/2009/01/26/element-in-view-event-plugin/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
+  jQuery('.my-counter-group').bind('inview', function (event, visible) {
+    if (visible == true) {
+      countUp();
+      jQuery('.my-counter-group').unbind('inview');
+    }
+  });
+});
+
+function countUpBy1(elem, max) {
+  const currentNumber = Math.floor(elem.html());
+  if (currentNumber >= max) {
+    return;
+  }
+  nextNumber = currentNumber + 1;
+  elem.html(nextNumber);
+
+  setTimeout(function () {
+    countUpBy1(elem, max);
+  }, 5);
+}
+
+function countUp() {
+  jQuery('.my-counter').each(function () {
+    var $this = jQuery(this);
+    var countTo = $this.html();
+    $this.html('0');
+    setTimeout(function() {
+      countUpBy1($this, countTo);
+    }, 5);
+  });
+}
