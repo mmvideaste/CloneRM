176b3ea42b70ad11657ff37135cfa0d42395b9b0
Albert Albala <albert@mediatribe.net>
2024-10-21 14:28:44 -0400

    Remove absolute urls (#6)

--- docs/xmlrpc0db0.php
+++ docs/xmlrpc0db0.php
@@ -4,12 +4,11 @@
 		<engineLink>https://wordpress.org/</engineLink>
 		<homePageLink>https://racheletmichel.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://racheletmichel.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://racheletmichel.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://racheletmichel.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://racheletmichel.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://racheletmichel.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="/wp-json/" />
 			</apis>
 	</service>
 </rsd>
-	
